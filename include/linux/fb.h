17e3e031adcec57af00a30d1feb48b199bde4fef
TheScarastic <warabhishek@gmail.com>
2018-03-23 13:49:02 +0530

    driver: video: mdss: Import tiffany changes

--- include/linux/fb.h
+++ include/linux/fb.h
@@ -531,7 +531,7 @@ struct fb_info {
 	} *apertures;
 
 	bool skip_vt_switch; /* no VT switch on suspend/resume required */
-#ifdef CONFIG_MACH_XIAOMI_TISSOT
+#if (defined CONFIG_MACH_XIAOMI_TISSOT) || (defined CONFIG_MACH_XIAOMI_TIFFANY)
 	int blank;
 #endif
 };
